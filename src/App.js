b1dd03897754a279332a23611c4406ad78baad47
louis <louis.z.j.li@oocl.com>
2018-07-18 17:01:17 +0800

    [louis] show todolist

--- src/App.js
+++ src/App.js
@@ -1,18 +1,26 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import TodoList from "./compoments/TodoList"
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="container">
+        <div>
+          <h2>Jquery To Do List</h2>
+          <p>
+            <em>Simple Todo List with adding and filter by diff status.</em>
+          </p>
+        </div>
+        <div >
+          <div>
+            <input class="input-text" type="text" name="ListItem" />
+            <div className="button" onclick="addItem()">Add</div>
+          </div>
+          <TodoList />
+
+        </div>
+
       </div>
     );
   }
---
+++ src/compoments/TodoList.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class TodoList extends Component {
+  render() {
+    return (
+      <div >
+        <ol>
+          <li class="checked">
+            <input name="done-todo"  type="checkbox" class="done-todo" />
+            FDGDJFKLGJ
+             </li>
+        </ol>
+      </div>
+    );
+  }
+}
+
+export default TodoList;
